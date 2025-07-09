a2518aa977b04caed7b4b8ffb2df89fd7786856e
SionLee1904 <sion1904@handong.ac.kr>
2025-07-09 22:38:51 +0900

    solve java

---
+++ LeeSiOn.java
@@ -0,0 +1,11 @@
+//:22200539 이시온
+
+import java.util.*;
+public class LeeSiOn {
+	public static void main (String [] args) {
+		System.out.println ("Name : LeeSiOn"); 
+		System.out.println ("University : Handong Global University"); 
+		System.out.println ("Student Number : 22200539"); 
+	}
+
+}
